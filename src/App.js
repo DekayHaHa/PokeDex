f98f88e6ecc028b817738968911d64fad715b503
Devin Kapla <devinkapla@gmail.com>
2020-03-07 21:06:44 -0700

    Get all types and weaknesses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { urlLanding } from './urlHolder'
-import { fetchData } from './fetchUtil'
+import { fetchData, getFilters } from './Util'
 import List from './List'
 import Header from './Header'
 
@@ -11,18 +11,24 @@ export default class App extends Component {
     this.state = {
       isLoading: false,
       pokemon: [],
+      pokemonTypes: [],
+      pokemonWeaknesses: []
     }
   }
   componentDidMount = () => {
     this.getData();
   }
+
   getData = async () => {
     const url = urlLanding();
     await this.changeState({ isLoading: true });
     const data = await fetchData(url);
     await this.changeState({ pokemon: data.pokemon });
+    await this.setFilters();
     await this.changeState({ isLoading: false });
   }
+
+  setFilters = () => this.changeState(getFilters(this.state.pokemon))
 
   changeState = newState => this.setState(newState)
 
@@ -31,7 +37,8 @@ export default class App extends Component {
     return (
       <article>
         <Header />
-        <List />
+        {console.log(this.state)}
+        <List pokemon={pokemon} />
       </article>
     );
   }
--- src/fetchUtil.js
+++
@@ -1,9 +0,0 @@
-export const fetchData = url => {
-  try {
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => data)
-  } catch (error) {
-    return error.message || error
-  }
-}
